44ad95c43f2a0c1fa42c772dac3a4133f4bfe74a
liudouA <1171258080@qq.com>
2018-07-06 12:22:18 +0800

    提交静态库项目

---
+++ podLibDemo/podLibDemo.h
@@ -0,0 +1,13 @@
+//
+//  podLibDemo.h
+//  podLibDemo
+//
+//  Created by 刘豆 on 2018/7/6.
+//  Copyright © 2018年 刘豆. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface podLibDemo : NSObject
++ (void)sayHello;
+@end
---
+++ podLibDemo/podLibDemo.m
@@ -0,0 +1,16 @@
+//
+//  podLibDemo.m
+//  podLibDemo
+//
+//  Created by 刘豆 on 2018/7/6.
+//  Copyright © 2018年 刘豆. All rights reserved.
+//
+
+#import "podLibDemo.h"
+
+@implementation podLibDemo
++ (void)sayHello{
+    
+    NSLog(@"sayhello");
+}
+@end
